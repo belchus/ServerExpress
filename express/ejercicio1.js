93b434d01736699ecdced363febf54ae80d7b9f7
belchus <arenabelu@gmail.com>
2022-12-15 21:17:07 -0300

    se guardaron los cambios en ejercicio1

--- express/ejercicio1.js
+++ express/ejercicio1.js
@@ -1,5 +1,6 @@
 
 const express = require ('express')
+const {response}= require('express')
 const app = express()
 const Contenedor = require('../desafio/app')
 
@@ -10,16 +11,39 @@ const server = app.listen(PORT, () => {
 })
 
 server.on('error', error => console.log(`Error:${error}`))
-
+//Codigo que funciona y muestra los productos
 const productos = new Contenedor('productos.txt')
+
+
+
+//Codigo para mostrar el error de productoRandom (linea 40)
+/*
+const productos = new Contenedor('productossss.txt')
+*/
 
 app.get('/productos', async (req, res) => {
     const productsList = await productos.getAll()
     res.send(productsList)
 })
 
-app.get('/productoRandom', async (req, res) => {
+//Codigo Anterior
+
+/*app.get('/productoRandom', async (req, res) => {
     const productsList = await productos.getAll()
-    const randomProduct = Math.floor(Math.random() * productsList.length)
+    const randomProduct = Math.floor(Math.random() * productsList.length)  
     res.send(productsList[randomProduct])
+    
 })
+*/
+
+
+//Codigo arreglado
+app.get('/productoRandom', async (req, res) => {
+    try {
+     const productsList = await productos.getAll()
+     const randomProduct = Math.floor(Math.random() * productsList.length)
+     res.send(productsList[randomProduct])
+    }catch(error) {
+     res.send("Producto no disponible")
+    }
+  })
